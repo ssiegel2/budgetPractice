3008373d49a9446c30f462ddef312a7b9621a843
Sam Siegel <ssiegel2@umbc.edu>
2016-07-31 11:59:35 -0400

    Fixed sidebar style

--- js/services.js
+++ js/services.js
@@ -2,10 +2,10 @@ var BudgetService = app.factory('BudgetService', [function($http)
 {
 
 	return {
-	
+
 		getCategory : function()
 		{
-			return $http.get("config/api/category")
+			return $http.get("api/category")
 				.then(function(result)
 				{
 					console.log("GET Success");
@@ -13,31 +13,31 @@ var BudgetService = app.factory('BudgetService', [function($http)
 					return result.data;
 				});
 		},
-		
+
 		postCategories : function(name)
 		{
-			$http.post("config/api/postCategories", {'name' : name})
+			$http.post("api/category", {'name' : name})
 				.then(function(data, status, headers, config)
 				{
 					console.log(data);
 					console.log("POST Success!");
 				});
 		},
-		
+
 		getExpenses : function(budgetID)
 		{
-			return $http.get("config/api.php?request=getExpense&budgetID=" + budgetID)
+			return $http.get("api/expense")
 				.then(function(result)
 				{
 					console.log("GET Success!");
 					console.log(result);
-					return result.data; 
+					return result.data;
 				});
 		},
-		
+
 		postExpenses : function(categoryID, expense, date, amount)
-		{	
-			$http.post("config/api.php?request=postExpense&budgetID=1", {'categoryID': categoryID ,'expense': expense, 
+		{
+			$http.post("config/api.php?request=postExpense&budgetID=1", {'categoryID': categoryID ,'expense': expense,
 			'date': date, 'amount': amount})
 				.then(function(data, status, headers, config)
 				{
@@ -45,7 +45,7 @@ var BudgetService = app.factory('BudgetService', [function($http)
 					console.log("POST Success!");
 				});
 		},
-		
+
 		getIncome : function(budgetID)
 		{
 			return $http.get("config/api.php?request=getIncome&budgetID=" + budgetID)
@@ -53,13 +53,13 @@ var BudgetService = app.factory('BudgetService', [function($http)
 				{
 					console.log("GET Success!");
 					console.log(result);
-					return result.data; 
+					return result.data;
 				});
 		},
-		
+
 		postIncome : function(categoryID, income, date, amount)
-		{	
-			$http.post("config/api.php?request=postIncome&budgetID=1", {'categoryID': categoryID , 'income': income, 
+		{
+			$http.post("config/api.php?request=postIncome&budgetID=1", {'categoryID': categoryID , 'income': income,
 			'date': date, 'amount': amount})
 				.then(function(data, status, headers, config)
 				{
@@ -67,7 +67,7 @@ var BudgetService = app.factory('BudgetService', [function($http)
 					console.log("POST Success!");
 				});
 		}
-	
+
 	}
-	
+
 }]);
